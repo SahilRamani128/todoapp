9688aa281ec5488e18a840b624c04cf2cb7a49f9
SahilRamani128 <sahilramani128@gmail.com>
2025-01-06 09:26:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Input from './components/Input';
 import List from './components/List';
 
 function App() {
-  const [ listTodo, setListTodo] = useState([]);
-  let addList = (inputText) => {
-    if(inputText!=='')
-      setListTodo([...listTodo, inputText]);
-  } 
+  // Retrieve data from local storage
+  const getLocalItems = () => {
+    const storedList = localStorage.getItem('Lists');
+    return storedList ? JSON.parse(storedList) : [];
+  };
 
-  const deleteListItem = (key)=>{
-    let newListTodo = [...listTodo];
-    newListTodo.splice(key,1)
-    setListTodo([...newListTodo])
-  }
+  const [listTodo, setListTodo] = useState(getLocalItems());
+
+  //save data in local storage
+  useEffect(() => {
+    localStorage.setItem('Lists', JSON.stringify(listTodo));
+  }, [listTodo]);
+
+  const addList = (inputText) => {
+    if (inputText !== '') {
+      setListTodo([...listTodo, inputText]);
+    }
+  };
+
+  const deleteListItem = (key) => {
+    const newListTodo = [...listTodo];
+    newListTodo.splice(key, 1);
+    setListTodo(newListTodo);
+  };
+
   return (
     <div className="Main-Container">
       <div className="center-container">
-        <Input addList = {addList}/>
+        <Input addList={addList} />
         <h1 className="app-heading">TODO</h1>
         <hr />
-        {listTodo.map((listItem,i)=>{
-          return(
-            <List key={i} index={i} item={listItem} deleteItem={deleteListItem} />
-          )
-        })}
+        {listTodo.map((listItem, i) => (
+          <List key={i} index={i} item={listItem} deleteItem={deleteListItem} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export  default App
+export default App;
