fdd8a943389ef82c8cc29db0e581856e40f8dc89
SahilRamani128 <sahilramani128@gmail.com>
2025-01-06 09:27:55 +0530

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,33 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Input(props) {
     const [ value, setValue] =  useState('');
     const handleEnterPress = (e)=>{
-        if(e.keyCode===13 /*13 is enterPress value*/){
+        //13 is enterPress value
+        if(e.keyCode===13){
             props.addList(value)
                 setValue("")
         }
     }
-  return (
-    <div className='input-container'>
-        <input 
-            type='text' 
-            className='input-box-todo' 
-            placeholder='Enter todo'
-            value={value}
-            onChange={e=>{
-                setValue(e.target.value)
-            }}
-            onKeyDown={handleEnterPress}
-        />
-        <button 
-            className='add-btn' 
-            onClick={()=> {
-                props.addList(value)
-                setValue("")
-        }}>+</button>
-    </div>
-  )
+    return (
+        <div className='input-container'>
+            <input 
+                type='text' 
+                className='input-box-todo' 
+                placeholder='Enter todo'
+                value={value}
+                onChange={e=>{
+                    setValue(e.target.value)
+                }}
+                onKeyDown={handleEnterPress}
+            />
+            <button 
+                className='add-btn' 
+                onClick={()=> {
+                    props.addList(value)
+                    setValue("")
+            }}>+</button>
+        </div>
+    )
 }
 
 export default Input
